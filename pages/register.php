750f0e859b30bd64ddf45df4f376bbe243980275
Tolsztajn <tamiol1@hotmail.com>
2017-11-07 12:36:40 -0300

    agrego

--- pages/register.php
+++ pages/register.php
@@ -4,6 +4,11 @@ $name = $_SESSION["inputsValues"]["name"]?? "";
 $surname = $_SESSION["inputsValues"]["surname"]?? "";
 $telefono = $_SESSION["inputsValues"]["telefono"]?? "";
 $mail = $_SESSION["inputsValues"]["mail"]?? "";
+
+if($_POST){
+  //ahora uso cosas del register controller
+
+}
  ?>
 
 <body>
@@ -20,7 +25,7 @@ $mail = $_SESSION["inputsValues"]["mail"]?? "";
 
               <?php endif ?>
           <?php unset($_SESSION["errores"]) ?>
-          <form class="registro" action="?pagina=register.controller" method="post" enctype="multipart/form-data">
+          <form class="registro" action="php/registercontroller.php" method="post" enctype="multipart/form-data">
           <!--Datos registro-->
           <input type="text" class="form-control" placeholder="Nombre" name="name" id="name" value=""><br>
           <input type="text" class="form-control" placeholder="Apellido" name="surname" id="surname" value=""><br>

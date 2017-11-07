67f0c6cddc4b655fbf6e0a61ad7d41571977d5bd
Tolsztajn <tamiol1@hotmail.com>
2017-11-07 12:04:01 -0300

    Agrego getters y setters

--- php/Clases/Usuario.php
+++ php/Clases/Usuario.php
@@ -44,9 +44,139 @@ class Usuario {
   }
 
 
+
+    /**
+     * Get the value of Name
+     *
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of Name
+     *
+     * @param mixed name
+     *
+     * @return self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Surname
+     *
+     * @return mixed
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * Set the value of Surname
+     *
+     * @param mixed surname
+     *
+     * @return self
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Telefno
+     *
+     * @return mixed
+     */
+    public function getTelefno()
+    {
+        return $this->telefno;
+    }
+
+    /**
+     * Set the value of Telefno
+     *
+     * @param mixed telefno
+     *
+     * @return self
+     */
+    public function setTelefno($telefno)
+    {
+        $this->telefno = $telefno;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Mail
+     *
+     * @return mixed
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Set the value of Mail
+     *
+     * @param mixed mail
+     *
+     * @return self
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Password
+     *
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Get the value of Db
+     *
+     * @return mixed
+     */
+    public function getDb()
+    {
+        return $this->db;
+    }
+
+    /**
+     * Set the value of Db
+     *
+     * @param mixed db
+     *
+     * @return self
+     */
+    public function setDb($db)
+    {
+        $this->db = $db;
+
+        return $this;
+    }
+
 }
 
-//faltan los getters y los setters
 
 
  ?>
